02838b7dd65332a230b7417107f4bfe48c4132f7
fadlyoktapriadi <fadlyoktapriadi41@gmail.com>
2024-09-26 09:22:19 +0700

    first init

---
+++ app/src/main/java/com/example/mybroadcastreceiver/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.mybroadcastreceiver
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mybroadcastreceiver.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener  {
+
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        binding?.btnPerm    ission?.setOnClickListener(this)
+
+    }
+
+    private var requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_permission -> requestPermissionLauncher.launch(android.Manifest.permission.RECEIVE_SMS)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
